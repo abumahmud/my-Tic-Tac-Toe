43ddc0f915e3f6bc9e48ded034394f92f1386bc7
abumahmud <alabiabdulbaasit@gmail.com>
2026-06-20 12:47:58 +0100

    Create square buttons

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,30 @@ import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
 import "./App.css";
 
-function App() {}
+function Board() {
+  const [value, setValue] = useState(Array(9).fill("null"));
 
-export default App;
+  return (
+    <>
+      <Squares value={value[0]} />
+      <Squares value={value[1]} />
+      <Squares value={value[2]} />
+      <Squares value={value[3]} />
+      <Squares value={value[4]} />
+      <Squares value={value[5]} />
+      <Squares value={value[6]} />
+      <Squares value={value[7]} />
+      <Squares value={value[8]} />
+    </>
+  );
+}
+
+function Squares({ value }) {
+  return (
+    <>
+      <button>{value}</button>
+    </>
+  );
+}
+
+export default Board;
